ff3edf16bc81a7425e0e5daf0ba4ddd7bc119ee5
Antony Budianto <antonybudianto@gmail.com>
2016-03-19 21:39:25 +0700

    use functional stateless component

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
 
-class App extends React.Component {
-  render () {
-    return <p> Hello React by {this.props.name}!</p>;
-  }
-}
+const App = ({name}) =>
+    <p> Hello React by {name}!</p>
 
 render(<App name='antony'/>, document.getElementById('app'));
