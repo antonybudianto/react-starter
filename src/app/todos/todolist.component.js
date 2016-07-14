a7c0b778fcd3818bf22b441919fe014a0ab9b085
Antony <antony.budianto@gdplabs.id>
2016-07-14 10:25:42 +0700

    add new eslint rule

--- src/app/todos/todolist.component.js
+++ src/app/todos/todolist.component.js
@@ -2,36 +2,36 @@ import React from 'react';
 import Todo from './todo.component';
 
 const Todolist = ({ allTodos, todos, showCompleted, onTodoClick, onChangeFilter}) => (
-    <div className='todolist'>
-        <label>
-            <input
-                type="checkbox"
-                defaultChecked={showCompleted}
-                onClick={onChangeFilter}
-            /> Show completed
-        </label>
-        <div>
-        {
-            todos.map(todo =>
-                <Todo
-                    key={todo.id}
-                    onTodoClick={() => onTodoClick(todo)}
-                    text={todo.text}
-                    completed={todo.completed}
-                    >
-                </Todo>
-            )
-        }
-        <div><strong>{todos.length}</strong> current items.
+  <div className='todolist'>
+    <label>
+      <input
+          type="checkbox"
+          defaultChecked={showCompleted}
+          onClick={onChangeFilter}
+      /> Show completed
+    </label>
+    <div>
+      {
+          todos.map(todo =>
+            <Todo
+                key={todo.id}
+                onTodoClick={() => onTodoClick(todo)}
+                text={todo.text}
+                completed={todo.completed}
+                >
+            </Todo>
+          )
+      }
+      <div><strong>{todos.length}</strong> current items.
         <strong> {allTodos.filter((todo) => todo.completed).length}</strong> completed items.
-        { allTodos.length !== todos.length ?
-            <span>
-                <strong> {allTodos.length}</strong> total items.
-            </span> : null
-        }
-        </div>
-        </div>
+      { allTodos.length !== todos.length ?
+        <span>
+          <strong> {allTodos.length}</strong> total items.
+        </span> : null
+      }
+      </div>
     </div>
+  </div>
 )
 
 export default Todolist;
